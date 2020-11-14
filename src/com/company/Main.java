1b9f2aa6655d89a16ed3756d778d26545df7b674
Selçuk Çalışkan <45595456+SelcukCaliskan@users.noreply.github.com>
2020-11-14 14:19:00 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("GitHub dan değişilik yaptım");
         System.out.println("GitHub dan 2. değişilik yaptım");
+	            System.out.println("GitHub dan 3. değişilik yaptım");
+
+	    
 
     }
 }
