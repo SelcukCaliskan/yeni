5f87f86a2c85abd4a2f636e091558c29d6873357
SelcukCaliskan <selcukcaliskan98@gmail.com>
2020-11-14 14:08:17 +0300

    2. değişiklik yaptım

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.printl("GitHub dan değişilik yaptım");
+	System.out.println("GitHub dan değişilik yaptım");
+        System.out.println("GitHub dan 2. değişilik yaptım");
+
     }
 }
